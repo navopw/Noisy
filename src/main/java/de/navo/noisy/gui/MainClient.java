4b3857174e2b78bcbd91b6ef22b36139b4a3710e
NAAVOO <stanislavh@protonmail.com>
2016-06-18 20:48:25 +0200

    Added plus and minus buttons to integer fields

--- src/main/java/de/navo/noisy/gui/MainClient.java
+++ src/main/java/de/navo/noisy/gui/MainClient.java
@@ -36,13 +36,19 @@ public class MainClient extends javax.swing.JFrame {
         imagePane = new javax.swing.JScrollPane();
         jLabel2 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        resolutionSlider = new javax.swing.JSlider();
         minimumHeightField = new javax.swing.JFormattedTextField();
         jLabel5 = new javax.swing.JLabel();
         maximumHeightField = new javax.swing.JFormattedTextField();
         jLabel6 = new javax.swing.JLabel();
         exportAsImageButton = new javax.swing.JButton();
         frequencySlider = new javax.swing.JSlider();
+        resolutionField = new javax.swing.JFormattedTextField();
+        resolutionPlusButton = new javax.swing.JButton();
+        resolutionMinusButton = new javax.swing.JButton();
+        minimumHeightPlusButton = new javax.swing.JButton();
+        minimumHeightMinusButton = new javax.swing.JButton();
+        maximumHeightPlusButton = new javax.swing.JButton();
+        maximumHeightMinusButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -67,18 +73,17 @@ public class MainClient extends javax.swing.JFrame {
 
         jLabel4.setText("Frequency:");
 
-        resolutionSlider.setMaximum(4096);
-        resolutionSlider.setMinimum(16);
-        resolutionSlider.setMinorTickSpacing(16);
-        resolutionSlider.setValue(16);
-
+        minimumHeightField.setEditable(false);
         minimumHeightField.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.NumberFormatter(new java.text.DecimalFormat("#0"))));
         minimumHeightField.setText("0");
+        minimumHeightField.setVerifyInputWhenFocusTarget(false);
 
         jLabel5.setText("Minimum height:");
 
+        maximumHeightField.setEditable(false);
         maximumHeightField.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.NumberFormatter(new java.text.DecimalFormat("#0"))));
         maximumHeightField.setText("255");
+        maximumHeightField.setVerifyInputWhenFocusTarget(false);
 
         jLabel6.setText("Maximum height:");
 
@@ -93,6 +98,53 @@ public class MainClient extends javax.swing.JFrame {
         frequencySlider.setMinimum(1);
         frequencySlider.setValue(6);
 
+        resolutionField.setEditable(false);
+        resolutionField.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.NumberFormatter(new java.text.DecimalFormat("#0"))));
+        resolutionField.setText("256");
+        resolutionField.setVerifyInputWhenFocusTarget(false);
+
+        resolutionPlusButton.setText("+");
+        resolutionPlusButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                resolutionPlusButtonActionPerformed(evt);
+            }
+        });
+
+        resolutionMinusButton.setText("-");
+        resolutionMinusButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                resolutionMinusButtonActionPerformed(evt);
+            }
+        });
+
+        minimumHeightPlusButton.setText("+");
+        minimumHeightPlusButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                minimumHeightPlusButtonActionPerformed(evt);
+            }
+        });
+
+        minimumHeightMinusButton.setText("-");
+        minimumHeightMinusButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                minimumHeightMinusButtonActionPerformed(evt);
+            }
+        });
+
+        maximumHeightPlusButton.setText("+");
+        maximumHeightPlusButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                maximumHeightPlusButtonActionPerformed(evt);
+            }
+        });
+
+        maximumHeightMinusButton.setText("-");
+        maximumHeightMinusButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                maximumHeightMinusButtonActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -105,28 +157,40 @@ public class MainClient extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jLabel5)
+                            .addComponent(jLabel6)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel4)
+                            .addComponent(minimumHeightField)
+                            .addComponent(maximumHeightField, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(exportAsImageButton, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                    .addComponent(resolutionSlider, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addComponent(jLabel5)
-                                        .addComponent(jLabel6)
-                                        .addComponent(jLabel2)
-                                        .addComponent(jLabel1)
-                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                            .addComponent(recalculateButton, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                            .addComponent(octavesSlider, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                            .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.LEADING))))
+                                .addComponent(minimumHeightPlusButton, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(4, 4, 4)
+                                .addComponent(minimumHeightMinusButton, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(0, 0, Short.MAX_VALUE))
-                            .addComponent(frequencySlider, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addContainerGap())
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addGap(0, 0, Short.MAX_VALUE)
+                                .addComponent(maximumHeightPlusButton, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(4, 4, 4)
+                                .addComponent(maximumHeightMinusButton, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE))))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(minimumHeightField, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(maximumHeightField, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(0, 0, Short.MAX_VALUE))))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel2)
+                                .addGap(0, 0, Short.MAX_VALUE))
+                            .addComponent(resolutionField))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(resolutionPlusButton, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(4, 4, 4)
+                        .addComponent(resolutionMinusButton, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(exportAsImageButton, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(recalculateButton, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(octavesSlider, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addComponent(frequencySlider, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -136,9 +200,12 @@ public class MainClient extends javax.swing.JFrame {
                     .addComponent(imagePane, javax.swing.GroupLayout.DEFAULT_SIZE, 512, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel2)
-                        .addGap(2, 2, 2)
-                        .addComponent(resolutionSlider, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(resolutionField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(resolutionPlusButton)
+                            .addComponent(resolutionMinusButton))
+                        .addGap(14, 14, 14)
                         .addComponent(jLabel1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(octavesSlider, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -149,11 +216,19 @@ public class MainClient extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jLabel5)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(minimumHeightField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(minimumHeightField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                .addComponent(minimumHeightPlusButton)
+                                .addComponent(minimumHeightMinusButton)))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jLabel6)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(maximumHeightField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(maximumHeightField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                .addComponent(maximumHeightPlusButton)
+                                .addComponent(maximumHeightMinusButton)))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(exportAsImageButton)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -175,7 +250,7 @@ public class MainClient extends javax.swing.JFrame {
 		FileChooser.open((file) -> {
 			if (file == null) return;
 			if (this.cachedImage == null) return;
-			
+
 			this.exportAsImage(file, (exception) -> {
 				if (exception == null) {
 					JOptionPane.showMessageDialog(null, "Exported noisemap successfully!", "Noisy - Information", JOptionPane.INFORMATION_MESSAGE);
@@ -186,17 +261,73 @@ public class MainClient extends javax.swing.JFrame {
 		});
     }//GEN-LAST:event_exportAsImageButtonActionPerformed
 
+    private void resolutionPlusButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_resolutionPlusButtonActionPerformed
+		int step = 128;
+		int field = Integer.parseInt(this.resolutionField.getText());
+		
+		if ((field + step) <= 2048) {
+			this.resolutionField.setValue(field + step);
+		}
+    }//GEN-LAST:event_resolutionPlusButtonActionPerformed
+
+    private void resolutionMinusButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_resolutionMinusButtonActionPerformed
+		int step = 128;
+		int field = Integer.parseInt(this.resolutionField.getText());
+		
+		if ((field - step) >= 16) {
+			this.resolutionField.setValue(field - step);
+		}
+    }//GEN-LAST:event_resolutionMinusButtonActionPerformed
+
+    private void minimumHeightPlusButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_minimumHeightPlusButtonActionPerformed
+		int step = 5;
+		int field = Integer.parseInt(this.minimumHeightField.getText());
+		
+		if ((field + step) <= 255) {
+			this.minimumHeightField.setValue(field + step);
+		}
+    }//GEN-LAST:event_minimumHeightPlusButtonActionPerformed
+
+    private void minimumHeightMinusButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_minimumHeightMinusButtonActionPerformed
+		int step = 5;
+		int field = Integer.parseInt(this.minimumHeightField.getText());
+		
+		if ((field - step) >= 0) {
+			this.minimumHeightField.setValue(field - step);
+		}
+    }//GEN-LAST:event_minimumHeightMinusButtonActionPerformed
+
+    private void maximumHeightPlusButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_maximumHeightPlusButtonActionPerformed
+		int step = 5;
+		int field = Integer.parseInt(this.maximumHeightField.getText());
+		
+		if ((field + step) <= 255) {
+			this.maximumHeightField.setValue(field + step);
+		}
+    }//GEN-LAST:event_maximumHeightPlusButtonActionPerformed
+
+    private void maximumHeightMinusButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_maximumHeightMinusButtonActionPerformed
+		int step = 5;
+		int field = Integer.parseInt(this.maximumHeightField.getText());
+		
+		if ((field - step) >= 0) {
+			this.maximumHeightField.setValue(field - step);
+		}
+    }//GEN-LAST:event_maximumHeightMinusButtonActionPerformed
+
 	public void start() {
 		this.setTitle("Noisy");
 		this.setLocationRelativeTo(null);
 		this.setResizable(false);
 		this.setVisible(true);
 
+		this.imagePane.requestFocus();
+		
 		this.recalculate();
 	}
 
 	private void recalculate() {
-		int resolution = this.resolutionSlider.getValue();
+		int resolution = Integer.parseInt(this.resolutionField.getText());
 		int octaves = this.octavesSlider.getValue();
 		int frequency = this.frequencySlider.getValue();
 
@@ -205,9 +336,7 @@ public class MainClient extends javax.swing.JFrame {
 		noise.setStartFrequencyX(frequency);
 		noise.setStartFrequencyY(frequency);
 
-		float[][] noiseMap = noise.calculate();
-
-		this.draw(noiseMap, resolution, resolution);
+		this.draw(noise.calculate(), resolution, resolution);
 	}
 
 	private void exportAsImage(File path, Consumer<Exception> callback) {
@@ -276,9 +405,15 @@ public class MainClient extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel6;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JFormattedTextField maximumHeightField;
+    private javax.swing.JButton maximumHeightMinusButton;
+    private javax.swing.JButton maximumHeightPlusButton;
     private javax.swing.JFormattedTextField minimumHeightField;
+    private javax.swing.JButton minimumHeightMinusButton;
+    private javax.swing.JButton minimumHeightPlusButton;
     private javax.swing.JSlider octavesSlider;
     private javax.swing.JButton recalculateButton;
-    private javax.swing.JSlider resolutionSlider;
+    private javax.swing.JFormattedTextField resolutionField;
+    private javax.swing.JButton resolutionMinusButton;
+    private javax.swing.JButton resolutionPlusButton;
     // End of variables declaration//GEN-END:variables
 }
--- src/main/java/de/navo/noisy/util/NMath.java
+++ src/main/java/de/navo/noisy/util/NMath.java
@@ -9,14 +9,5 @@ public class NMath {
 	public static double clamp(double value, double min, double max) {
 		return (value < min) ? min : (value > max) ? max : value;
 	}
-	
-	public static double navo(double value, double valueMin, double valueMax, double min, double max) {
-		double mid = Interpolation.linearInterpolate(value, valueMin, valueMax);
-		double percent = valueMax / mid;
-		
-		double endValue = percent * max;
-		
-		return clamp(endValue, min, max);
-	}
 
 }
